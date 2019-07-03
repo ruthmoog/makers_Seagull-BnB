3b3565adafd7b34031f633978e2b09b2355045d0
joseph townshend <joetownshend@mac.com>
2019-07-03 14:37:58 +0100

    Add functionality for .create and tests.

--- lib/listing.rb
+++ lib/listing.rb
@@ -1,7 +1,7 @@
 class Listing
   attr_reader :name, :description
 
-def initialize(name, description)
+def initialize(name:, description:)
   @name = name
   @description = description
   @reserved = false
@@ -11,8 +11,13 @@ def reserved?
   @reserved
 end
 
-
-
+def self.create(name:, description:)
+  listing = Listing.new(name: name, description: description)
+  $array.push(listing)
+  listing
+end
 
 
 end
+
+$array = []
--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -2,10 +2,23 @@ require 'listing'
 
 describe Listing do
   it 'can create a new listing' do
-    listing = Listing.new('Test Name', 'Test description')
+    listing = Listing.new(name: 'Test Name', description: 'Test description')
 
     expect(listing.name).to eq 'Test Name'
     expect(listing.description).to eq 'Test description'
     expect(listing.reserved?).to eq false
   end
+
+  describe '.create' do
+    it 'Can create a new instance of listing' do
+      listing = Listing.create(name: 'Seagull Cottage', description: '4 Stars')
+
+      expect(listing).to be_a Listing
+      expect(listing.name).to eq('Seagull Cottage')
+      expect(listing.description).to eq('4 Stars')
+      expect($array[0]).to eq listing
+    end
+  end
+
+
 end
