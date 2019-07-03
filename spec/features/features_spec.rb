bb86ef8bc7e688d16cd86b188642889cb9d0d5a6
Lucian Mot <lucian.i.mot@gmail.com>
2019-07-03 15:43:42 +0100

    caw caw

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,17 @@
+feature 'Hello Seagull!' do
+  scenario "when visiting index should print Hello Seagull!" do
+    visit('/')
+    expect(page).to have_content "Welcome to Seagull BnB"
+  end
+
+  scenario "visit index page" do
+    visit('/')
+    find_link('Display').visible?
+    expect(page).to have_content('Display')
+  end
+
+
+
+
+
+end
