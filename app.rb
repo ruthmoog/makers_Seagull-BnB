5a4e19c7abc28d17d737d09f2da048289a76779d
Ben Scott <bengscott2@gmail.com>
2019-07-04 10:35:27 +0100

    add listings view

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
+require_relative './lib/listing.rb'
 
 class Listing_manager <Sinatra::Base
 
   get '/' do
     erb :'index'
+  end
+
+  get '/listings' do
+    @listings = $array
+    erb :'listings'
   end
 
   run! if app_file == $0
