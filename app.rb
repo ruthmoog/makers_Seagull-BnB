35a35d07840063e431885f5cf320f1c5e8da4a29
Lucian Mot <lucian.i.mot@gmail.com>
2019-07-04 10:34:48 +0100

    initial creation of add new listing view and method

--- app.rb
+++ app.rb
@@ -6,5 +6,14 @@ class Listing_manager <Sinatra::Base
     erb :'index'
   end
 
+  get '/listings/new' do
+    erb :'listings'
+  end
+
+  post '/listings' do
+    Listing.create(name: params[:name], description: params[:description])
+    redirect :listings
+  end
+
   run! if app_file == $0
 end
