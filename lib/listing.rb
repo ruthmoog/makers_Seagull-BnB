4a3c435a6001d45229f60508e05573882c37277c
Ruth Baker <ruthmoog@gmail.com>
2019-07-04 12:15:32 +0100

    Use test db for tests, else, db

--- lib/listing.rb
+++ lib/listing.rb
@@ -4,31 +4,35 @@ $array = []
 class Listing
   attr_reader :name, :description
 
-def initialize(name:, description:)
-  @name = name
-  @description = description
-  @reserved = false
-end
+  def initialize(name:, description:)
+    @name = name
+    @description = description
+    @reserved = false
+  end
 
-def reserved?
-  @reserved
-end
+  def reserved?
+    @reserved
+  end
 
-def self.create(name:, description:)
-  listing = Listing.new(name: name, description: description)
-  $array.push(listing)
-  listing
-end
+  def self.create(name:, description:)
+    listing = Listing.new(name: name, description: description)
+    $array.push(listing)
+    listing
+  end
 
-def self.all
-  connection = PG.connect(dbname: 'seagull_test')
-  result = connection.exec('SELECT * FROM listings')
-  result.map { |listing| listing['name'] }
-end
+  def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'seagull_test')
+    else
+      connection = PG.connect(dbname: 'seagull')
+    end
 
-def reserve_switch
-  reserved? ? @reserved = false : @reserved = true
-end
+    result = connection.exec('SELECT * FROM listings')
+    result.map { |listing| listing['name'] }
+  end
 
+  def reserve_switch
+    reserved? ? @reserved = false : @reserved = true
+  end
 
 end
